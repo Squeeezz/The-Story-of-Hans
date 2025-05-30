515e19fa7c39cca89128332023eafa83ba549bad
Gabe114 <killereye65@outlook.de>
2025-05-30 10:30:01 +0000

    ein schaden system eingerichtet + ein test Item errichtet

--- TheStoryOfHans/item.java
+++ TheStoryOfHans/item.java
@@ -4,7 +4,9 @@ public class item {
     public String itemBeschreibung;
     public int itemWert;
 
-   
+
+    // TEST ITEM
+    public item rostmesser = new item("kleiner roestiger Messer" ," kann schneiden, ist leicht zerbrechbar, ist nicht sehr effektiv im kampf", 2);
 
     // Konstruktor
     public item(String itemName, String itemBeschreibung, int itemWert) {
